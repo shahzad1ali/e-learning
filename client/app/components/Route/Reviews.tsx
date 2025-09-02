9f300aee834668dc816d512d1bcb342fc5c93582
shahzad1ali <shahzadmobi619@gmail.com>
2025-09-02 19:24:49 +0500

    update logout function

--- client/app/components/Route/Reviews.tsx
+++ client/app/components/Route/Reviews.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import React from 'react'
 import ReviewCard from "../Review/ReviewCard"
 import { useTheme } from 'next-themes'
+import img1 from "../../../public/assets/review/1 (1).jpg";
 
 type Props = {}
 
@@ -10,7 +11,7 @@ type Props = {}
 export const reviews = [
   {
     name: "Gene Bates",
-    avatar: "https://randomuser.me/api/portraits/men/1.jpg",
+    avatar: "img1.jpg",
     profession: "Student | Cambridge university",
     comment: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   },
