e0e3918c312d251549d91396e6c0fc2ff316d543
shahzad1ali <shahzadmobi619@gmail.com>
2025-09-02 13:24:55 +0500

    vercel url added

--- client/app/components/Loader/Loader.tsx
+++ client/app/components/Loader/Loader.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import "./loader.css";
-
+import React from "react";
+// file reanamed
 const Loader = () => {
   return (
     <div className="flex justify-center items-center h-screen">
